021158bb470a069779374831f14877b15252f304
caphekoduong <61449974+caphekoduong@users.noreply.github.com>
2020-10-19 19:00:00 -0700

    Add files via upload

---
+++ Models/WeightManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_THC_Inventory.Models
+{
+    public class WeightManager
+    {
+        public long Id { get; set; }
+        public string Ticketnum { get; set; }
+        public string Truckno { get; set; }
+        public string Prodname { get; set; }
+        public string Custname { get; set; }
+        public DateTime Date_in { get; set; }
+        public DateTime Date_out { get; set;}
+        public int FirstWeight { get; set; }
+        public int SecondWeight { get; set; }
+        public string Note { get; set; }
+
+    }
+}
---
+++ Models/WeightManagerContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_THC_Inventory.Models
+{
+    public class WeightManagerContext: DbContext
+    {
+        public WeightManagerContext (DbContextOptions<WeightManagerContext> options) : base(options)
+        {
+
+        }
+        public DbSet<WeightManager> WeightManagers { get; set; }
+
+    }
+}
